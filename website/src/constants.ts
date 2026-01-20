3b13e9cc146f805f77c35f452f486ff509f8ea57
Ignasi Barrera <ignasi@tetrate.io>
2026-01-20 15:59:22 +0100

    extract common vars
    Signed-off-by: Ignasi Barrera <ignasi@tetrate.io>

---
+++ website/src/constants.ts
@@ -0,0 +1,5 @@
+// Global constants for the Envoy Ecosystem website
+
+export const GITHUB_REPO = 'https://github.com/tetratelabs/envoy-ecosystem';
+export const GITHUB_REPO_RAW = 'tetratelabs/envoy-ecosystem';
+export const EXTENSIONS_PATH = 'extensions';
