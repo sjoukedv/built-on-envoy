b7951b450df5315cf92e598531871b9deac6a775
Ignasi Barrera <ignasi@tetrate.io>
2026-01-22 02:03:12 +0100

    cli: bootstrap the CLI and the `ee run` command (#1)
    Bootstraps the CLI with the `ee run` command that starts a default Envoy
    proxying httpbin as a test upstream.
    
    ---------
    
    Signed-off-by: Ignasi Barrera <ignasi@tetrate.io>

---
+++ cli/e2e/main_test.go
@@ -0,0 +1,35 @@
+// Copyright Envoy Ecosystem
+// SPDX-License-Identifier: Apache-2.0
+// The full text of the Apache license is available in the LICENSE file at
+// the root of the repo.
+
+package e2e
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	internaltesting "github.com/tetratelabs/envoy-ecosystem/cli/internal/testing"
+)
+
+var cliBin string
+
+const proxyPort = 10000
+
+func TestMain(m *testing.M) {
+	var err error
+	cliBin, err = buildCLIOnDemand()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to build CLI binary: %v\n", err)
+		os.Exit(1)
+	}
+
+	os.Exit(m.Run())
+}
+
+// buildCLIOnDemand builds the CLI binary unless CLI_BIN is set.
+// If CLI_BIN environment variable is set, it will use that path instead.
+func buildCLIOnDemand() (string, error) {
+	return internaltesting.BuildGoBinaryOnDemand("CLI_BIN", "ee", ".")
+}
